e00193facded963c50fdb975bcb282d186ec2d85
Rubaid07 <rubaidindu@gmail.com>
2025-08-18 12:20:43 +0600

    problem 6 solved

---
+++ problem6.js
@@ -0,0 +1,11 @@
+// Sum of All Numbers in an Array
+
+const sumOfArr = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i]
+    }
+    return sum;
+}
+const number = [1, 2, 3, 4]
+console.log(sumOfArr(number));
